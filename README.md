1b54da5be67e88d1160a238dfe52c662081d33de
AbdurRob <abdurrobr34l@gmail.com>
2026-02-26 19:08:54 +0600

    Basic work done

---
+++ README.md
@@ -0,0 +1,3 @@
+```
+TaskPulse: A modern task management dashboard built with React, Vite, TailwindCSS, and DaisyUI. Features JWT-based authentication, protected routes, and REST API data visualization for users, analytics, and products.
+```
---
+++ src/Routes/MainRoute.jsx
@@ -0,0 +1,51 @@
+import { createBrowserRouter } from "react-router";
+import Login from "../Pages/Login";
+import DashboardLayout from "../Layouts/DashboardLayout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/dashboard",
+    element: <DashboardLayout />,
+    children: [
+      {
+        index: true,
+        element: <Dashboard />,
+      },
+    ],
+  },
+]);
+
+export default router;
+
+// import { createBrowserRouter } from "react-router";
+// import Login from "../Pages/Login";
+// import DashboardLayout from "../Layouts/DashboardLayout";
+// import Dashboard from "../Pages/Dashboard";
+// import PrivateRoute from "./PrivateRoute";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Login />,
+//   },
+//   {
+//     path: "/dashboard",
+//     element: (
+//       <PrivateRoute>
+//         <DashboardLayout />
+//       </PrivateRoute>
+//     ),
+//     children: [
+//       {
+//         index: true,
+//         element: <Dashboard />,
+//       },
+//     ],
+//   },
+// ]);
+
+// export default router;
