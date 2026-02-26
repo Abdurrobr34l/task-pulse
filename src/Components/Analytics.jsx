25ae8f9dd2934473e0fc0d0faee6cbe9292a4bcf
AbdurRob <abdurrobr34l@gmail.com>
2026-02-27 02:57:17 +0600

    Analytic card completed, not responsive yet

---
+++ src/Components/Analytics.jsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from "react";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Tooltip,
+} from "chart.js";
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip);
+
+const Analytics = () => {
+  const [analytics, setAnalytics] = useState([]);
+
+  useEffect(() => {
+    fetch("https://task-api-eight-flax.vercel.app/api/analytics")
+      .then((res) => res.json())
+      .then((data) => setAnalytics(data));
+  }, []);
+
+  // Convert API data (dynamic)
+  const labels = analytics.map((a) =>
+    new Date(a.date).toLocaleDateString("en-US", { weekday: "short" }).charAt(0)
+  );
+
+  const values = analytics.map((a) => a.views);
+
+  const maxValue = Math.max(...values);
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        data: values,
+        borderRadius: 50,
+        borderSkipped: false,
+        backgroundColor: values.map((v) =>
+          v === maxValue ? "#14532d" : "#2f855a"
+        ),
+        hoverBackgroundColor: "#0f3d2a",
+        barThickness: 40,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: { display: false },
+      tooltip: {
+        backgroundColor: "#1a5d3e",
+        displayColors: false,
+      },
+    },
+    scales: {
+      x: {
+        grid: { display: false },
+        border: { display: false },
+        ticks: {
+          color: "#6b7280",
+          font: { size: 14 },
+        },
+      },
+      y: {
+        display: false,
+        grid: { display: false },
+        border: { display: false },
+      },
+    },
+  };
+
+  return (
+    <div className="bg-white rounded-3xl p-8">
+      <h2 className="text-2xl font-semibold text-gray-800 mb-6">
+        Project Analytics
+      </h2>
+
+      <div className="h-72">
+        <Bar data={data} options={options} />
+      </div>
+    </div>
+  );
+};
+
+export default Analytics;
--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Overview from '../Components/Overview';
+import Analytics from '../Components/Analytics';
 
 const Dashboard = () => {
   return (
     <div className='border'>
       <Overview/>
+      
+      <div className='mt-3'>
+        <Analytics/>
+      </div>
     </div>
   );
 };
